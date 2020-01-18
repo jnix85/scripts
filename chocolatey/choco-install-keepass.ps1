f7ebe4c472fd854f0c094036c33a06039e0489df
Jason Parks <jparks@jpconsulted.com>
2020-01-17 20:38:59 -0600

    .

---
+++ chocolatey/choco-install-keepass.ps1
@@ -0,0 +1,4 @@
+choco install -y keepass.install
+choco install -y keepass-plugin-qrcodegen keepass-plugin-rdp keepass-plugin-favicon keepass-plugin-winkee keepass-plugin-databasebackup keepass-plugin-otpkeyprov keepass-plugin-gost keepass-plugin-keeanywhere keepass-plugin-keetheme keepass-plugin-twofishcipher keepass-plugin-mskeyimporter
+
+
